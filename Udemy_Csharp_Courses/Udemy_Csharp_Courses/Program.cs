7477340e74f544d1631fb7eee79e80a71856996e
Robert <Robert@DESKTOP-PCFCJQN>
2021-10-10 09:01:49 +0600

    New Courses

---
+++ Udemy_Csharp_Courses/Udemy_Csharp_Courses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Udemy_Csharp_Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
